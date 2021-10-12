48d15c8bd410c711da1b768daca8120b8a9be50d
Estefania Piñero <61096992+EstefiPinero@users.noreply.github.com>
2021-10-12 19:44:20 -0300

    Delete Proyecto Piñero/js directory

--- Proyecto Piñero/js/stockParcelas.js
+++
@@ -1,7 +0,0 @@
-//////////////////////// Defino objeto con los datos de la parcela////////////////////////////////////////////////
-//los guardo en un array
-let parcelas = [
-
-    
-]; 
-console.log(parcelas);
